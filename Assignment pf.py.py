6f2efbde474de8c492511436f70d0e9f93f6e183
student-sania <153992538+student-sania@users.noreply.github.com>
2023-12-21 20:45:13 +0500

    Add files via upload

---
+++ Assignment pf.py.py
@@ -0,0 +1,28 @@
+
+#list = [1,2,2,3,4,4,5,6]
+#empty_list = [ ]
+#for i in list:
+#    if i not in empty_list:
+#       empty_list.append(i)
+ #       list.clear()
+ #       print(empty_list)
+# program no:1
+number=[1,1,2,3,3,4,5,5]
+for i in range(1,5+1):
+    if i == i:
+        print(i)
+    else:
+        print("try again")
+
+# program no:2
+a = [[1,3],[6,4]]   
+b = [[3,5],[3,2]]     
+for i in range(1):
+    for j in range(1):
+        c = a[i][j]+b[i][j]
+        print("the sum of matrix=",c)
+else:
+    print()        
+
+
+
